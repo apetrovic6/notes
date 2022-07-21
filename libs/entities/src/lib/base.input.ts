5d0ff55c66b5d84c289d3121781ba2a79e3d71b7
apetrovic6 <petrovicante6@gmail.com>
2022-07-21 09:06:44 +0200

    refactor: implement base input

---
+++ libs/entities/src/lib/base.input.ts
@@ -0,0 +1,8 @@
+import {InputType, Field, ID} from '@nestjs/graphql';
+
+
+@InputType()
+export class BaseInput {
+  @Field(returns => ID, { name: 'id', description: 'Unique identifier' })
+  id: string
+}
