937401aa8decbe22fca9529a9c5d3f3c046756ba
apetrovic6 <petrovicante6@gmail.com>
2022-07-21 08:38:46 +0200

    feat: set up base entities lib

---
+++ libs/entities/src/lib/base.entity.ts
@@ -0,0 +1,17 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {Field, ID, ObjectType} from "@nestjs/graphql";
+
+@ObjectType()
+export class BaseEntity {
+  @Field(returns => ID, { name: 'id', description: 'Unique identifier' })
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Field(() => Date, { description: 'Date of creation' })
+  @Column()
+  createdAt: Date;
+
+  // @Field(() => Date, { description: 'Last updated' })
+  // @Column()
+  // updatedAt: Date;
+}
