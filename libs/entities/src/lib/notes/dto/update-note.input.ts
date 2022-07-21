4141160ff776b8afe593a6ae80853f204aa3c093
apetrovic6 <petrovicante6@gmail.com>
2022-07-21 09:08:21 +0200

    refactor: not needed, since inheritance

--- libs/entities/src/lib/notes/dto/update-note.input.ts
+++ libs/entities/src/lib/notes/dto/update-note.input.ts
@@ -3,6 +3,5 @@ import {InputType, Field, PartialType, ID} from '@nestjs/graphql';
 
 @InputType()
 export class UpdateNoteInput extends PartialType(CreateNoteInput) {
-  @Field(() => ID, { description: 'Id of the note' })
-  id: string;
+
 }
