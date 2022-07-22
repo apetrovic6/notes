304c2ddef74cf95f05b302aea84aa68fb9a9d7ac
apetrovic6 <petrovicante6@gmail.com>
2022-07-22 15:27:30 +0200

    feat: init user resource

---
+++ apps/server/src/user/user.resolver.ts
@@ -0,0 +1,33 @@
+import {Resolver, Query, Mutation, Args, Int, ID} from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { CreateUserInput, UpdateUserInput, User } from "@notes-app/entities";
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => User)
+  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.userService.create(createUserInput);
+  }
+
+  @Query(() => [User], { name: 'user' })
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Query(() => User, { name: 'user' })
+  findOne(@Args('id', { type: () => ID }) id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
+    return this.userService.update(updateUserInput.id, updateUserInput);
+  }
+
+  @Mutation(() => User)
+  removeUser(@Args('id', { type: () => ID }) id: string) {
+    return this.userService.remove(id);
+  }
+}
