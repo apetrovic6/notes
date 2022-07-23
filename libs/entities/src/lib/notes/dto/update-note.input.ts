f2d96cee4000eccc776bc0b6d3f3df8754d1fe99
apetrovic6 <petrovicante6@gmail.com>
2022-07-23 10:42:08 +0200

    fix: add id field to UpdateNoteInput

--- libs/entities/src/lib/notes/dto/update-note.input.ts
+++ libs/entities/src/lib/notes/dto/update-note.input.ts
@@ -3,5 +3,6 @@ import {InputType, Field, PartialType, ID} from '@nestjs/graphql';
 
 @InputType()
 export class UpdateNoteInput extends PartialType(CreateNoteInput) {
-
+  @Field(returns => ID, { name: 'id', description: 'Unique identifier' })
+  id: string
 }
