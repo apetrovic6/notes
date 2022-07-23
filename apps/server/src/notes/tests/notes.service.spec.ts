ddb425328d7962695de10aaf411a535cd3b0f01f
apetrovic6 <petrovicante6@gmail.com>
2022-07-23 18:25:59 +0200

    test: initialized notes-service.spec

--- apps/server/src/notes/tests/notes.service.spec.ts
+++ apps/server/src/notes/tests/notes.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { NotesService } from './notes.service';
+import { NotesService } from '../notes.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Note } from '@notes-app/entities';
 
 describe('NotesService', () => {
   let service: NotesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotesService],
+      providers: [
+        NotesService,
+        { provide: getRepositoryToken(Note), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<NotesService>(NotesService);
