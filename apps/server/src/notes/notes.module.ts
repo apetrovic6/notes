194f010a3cf4f4546ab8b657c6f2c8c4e0bf2714
apetrovic6 <petrovicante6@gmail.com>
2022-07-23 10:36:28 +0200

    chore: Refactor Field Resolver logic into separate module to mitigate circular dependencies.

--- apps/server/src/notes/notes.module.ts
+++ apps/server/src/notes/notes.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { NotesResolver } from './notes.resolver';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {Note} from "@notes-app/entities";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Note } from "@notes-app/entities";
+import { NotesUserFieldResolverModule } from "../notes-user-field-resolver/notes-user-field-resolver.module";
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Note])],
+  imports: [TypeOrmModule.forFeature([Note]), NotesUserFieldResolverModule],
   providers: [NotesResolver, NotesService],
-  exports: [],
+  exports: [NotesService],
 })
 export class NotesModule {}
