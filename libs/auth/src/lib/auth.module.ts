a7df21012513bf883bf126121e25d3d2e9676eb4
apetrovic6 <petrovicante6@gmail.com>
2022-07-23 18:26:59 +0200

    feat: init auth lib

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Module({
-  controllers: [],
-  providers: [],
+  providers: [AuthService],
   exports: [],
 })
 export class AuthModule {}
