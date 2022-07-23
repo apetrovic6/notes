b038537687b442ece2ccd51e5ce1f8204814adb7
apetrovic6 <petrovicante6@gmail.com>
2022-07-23 18:19:24 +0200

    test: setup notes mockups and stubs for testing

---
+++ apps/server/src/notes/__mocks__/notes.service.ts
@@ -0,0 +1,10 @@
+import { fn } from 'jest-mock';
+import { noteStub } from '../tests/stubs/note.stub';
+
+export const NotesService = fn().mockReturnValue({
+  create: fn().mockReturnValue(noteStub()),
+  findOne: fn().mockReturnValue(noteStub()),
+  findAll: fn().mockReturnValue([noteStub()]),
+  update: fn().mockReturnValue(noteStub()),
+  remove: fn().mockReturnValue(null),
+});
---
+++ apps/server/src/notes/tests/stubs/note.stub.ts
@@ -0,0 +1,10 @@
+import { Note } from '@notes-app/entities';
+import { userStub } from '../../../user/tests/stubs/user.stub';
+
+export const noteStub = (): Note => ({
+  id: 'd0594ffa-bada-4bfe-bf14-7c44ae110f38',
+  title: 'Test Note',
+  content: 'This is a test note',
+  user: userStub(),
+  createdAt: new Date('2022-07-23T00:00:00.000Z'),
+});
