330211be2460d60fece73538022fe8d61b7e98ca
apetrovic6 <petrovicante6@gmail.com>
2022-07-23 10:44:00 +0200

    fix: named CreateUserInput

--- libs/entities/src/lib/user/dto/create-user.input.ts
+++ libs/entities/src/lib/user/dto/create-user.input.ts
@@ -1,7 +1,7 @@
 import {InputType, Int, Field, OmitType} from '@nestjs/graphql';
 import { BaseInput } from "../../base.input";
 
-@InputType()
+@InputType("UserInput")
 export class CreateUserInput extends OmitType(BaseInput, ["id"] as const) {
   @Field(() => String, { description: 'User email' })
   email: string;
