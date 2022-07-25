0a3a73b0ddcb591e7042f842372b1cfa854279d4
apetrovic6 <petrovicante6@gmail.com>
2022-07-25 13:59:43 +0200

    feat: login mutation

--- apps/server/src/auth/auth.resolver.ts
+++ apps/server/src/auth/auth.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { CreateUserInput, User } from '@notes-app/entities';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
@@ -12,5 +13,12 @@ export class AuthResolver {
     userArgs: CreateUserInput
   ): Observable<User> {
     return this.authService.signup(userArgs);
+  }
+
+  @Mutation(() => User, { name: 'login' })
+  signin(
+    @Args('authArgs', { type: () => CreateUserInput }) authArgs: CreateUserInput
+  ): Observable<User> {
+    return this.authService.signin(authArgs);
   }
 }
