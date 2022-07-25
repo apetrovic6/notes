ddb8e15f053a6f86de68310acda64352fe200b23
apetrovic6 <petrovicante6@gmail.com>
2022-07-25 14:05:00 +0200

    feat: init PasswordService in AuthHelpersModule

--- libs/auth-helpers/src/lib/auth-helpers.module.ts
+++ libs/auth-helpers/src/lib/auth-helpers.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PasswordService } from './password.service';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  providers: [PasswordService],
+  exports: [PasswordService],
 })
 export class AuthHelpersModule {}
