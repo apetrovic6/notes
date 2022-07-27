2414ca267755e236e826d1841622aa6b5f2288ce
apetrovic6 <petrovicante6@gmail.com>
2022-07-27 19:36:14 +0200

    feat: add DataLoaderService

---
+++ apps/server/src/data-loader/data-loader.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { NotesService } from '../notes/notes.service';
+import { UserService } from '../user/user.service';
+import { Note, User } from '@notes-app/entities';
+import DataLoader from 'dataloader';
+
+@Injectable()
+export class DataLoaderService {
+  constructor(
+    private readonly notesService: NotesService,
+    private readonly userService: UserService
+  ) {}
+
+  getLoaders() {
+    return {
+      notesLoader: this._createNotesLoader(),
+      userLoader: this._createUserLoader(),
+    };
+  }
+
+  private _createNotesLoader() {
+    return new DataLoader<string, Note[]>(
+      async (keys: readonly string[]) =>
+        await this.notesService.loadNotes(keys as string[])
+    );
+  }
+
+  private _createUserLoader() {
+    return new DataLoader<string, User>(
+      async (keys: readonly string[]) =>
+        await this.userService.loadUsers(keys as string[])
+    );
+  }
+}
