f1ddc435a54bc01f0d548aa15e5e6d8c58567d2d
apetrovic6 <petrovicante6@gmail.com>
2022-07-29 00:16:03 +0200

    test: AuthService test

--- apps/server/src/auth/tests/auth.service.spec.ts
+++ apps/server/src/auth/tests/auth.service.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from '../auth.service';
 import { PasswordService } from '@notes/auth-helpers';
+import { userStub } from '../../user/tests/stubs/user.stub';
+
+jest.mock('../auth.service');
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -15,5 +18,34 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('When signup is called', () => {
+    let user;
+
+    beforeEach(() => {
+      const userArgs = {
+        email: userStub().email,
+        password: userStub().password,
+      };
+
+      service.signup(userArgs).subscribe(data => (user = data));
+    });
+
+    it('It should be defined', () => {
+      expect(service.signup).toBeDefined();
+    });
+
+    it("It should call service's signup method with user's email and password", () => {
+      const { email, password } = userStub();
+      expect(service.signup).toHaveBeenCalledWith({
+        email,
+        password,
+      });
+    });
+
+    it('It should return user', () => {
+      expect(user).toEqual(userStub());
+    });
   });
 });
