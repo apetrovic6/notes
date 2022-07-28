8e8352ea6b3ce957d243b2958e94446fca9dda91
apetrovic6 <petrovicante6@gmail.com>
2022-07-29 00:17:35 +0200

    chore: Import UserModule into AuthModule

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { AuthHelpersModule } from '@notes/auth-helpers';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [AuthHelpersModule],
+  imports: [AuthHelpersModule, UserModule],
   providers: [AuthResolver, AuthService],
   exports: [AuthService],
 })
