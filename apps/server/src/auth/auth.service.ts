f17e2ef6770a56fa982b74d570926ac1b6bd28fd
apetrovic6 <petrovicante6@gmail.com>
2022-07-29 00:16:40 +0200

    feat: implement AuthService signup and signin methods

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -2,17 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from '@notes/entities/user';
 import { from, map, switchMap } from 'rxjs';
 import { PasswordService } from '@notes/auth-helpers';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly passwordService: PasswordService) {}
+  constructor(
+    private readonly passwordService: PasswordService,
+    private readonly userService: UserService
+  ) {}
 
   signup(userArgs: CreateUserInput) {
-    return from(of(userStub()));
+    return from(this.passwordService.hashPassword(userArgs.password)).pipe(
+      map(hash => (userArgs.password = hash)),
+      switchMap(() => this.userService.create(userArgs))
+    );
   }
 
   signin(authArgs: CreateUserInput) {
-    return from(of(userStub()));
+    return from(this.userService.findByEmail(authArgs.email)).pipe(
+      switchMap(user =>
+        this.passwordService.verifyPassword(user.password, authArgs.password)
+      )
+    );
   }
 
   validateUser() {
