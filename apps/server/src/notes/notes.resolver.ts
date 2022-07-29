9c539382a670bdbeaab458e9b87d1a9eeb51ef1e
apetrovic6 <petrovicante6@gmail.com>
2022-07-29 11:22:32 +0200

    feat: Implement JwtAuthGuard

--- apps/server/src/notes/notes.resolver.ts
+++ apps/server/src/notes/notes.resolver.ts
@@ -7,16 +7,19 @@ import {
   ResolveField,
   Parent,
 } from '@nestjs/graphql';
-import { NotesService } from './notes.service';
+import { UseGuards } from '@nestjs/common';
 import { CreateNoteInput, Note, UpdateNoteInput } from '@notes/entities/notes';
 import { User } from '@notes/entities/user';
+import { JwtAuthGuard } from '@notes/auth-helpers';
 import { EDataLoader, UserLoader } from '../data-loader/IDataLoaders';
+import { NotesService } from './notes.service';
 import { Loader } from '../data-loader/decorators/loader.decorator';
 
 @Resolver(() => Note)
 export class NotesResolver {
   constructor(private readonly notesService: NotesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => Note)
   createNote(@Args('createNoteInput') createNoteInput: CreateNoteInput) {
     return this.notesService.create(createNoteInput);
