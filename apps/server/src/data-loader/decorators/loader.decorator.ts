2800063a29b75277bad49f51b02ae5a9d3800a2c
apetrovic6 <petrovicante6@gmail.com>
2022-07-29 08:46:10 +0200

    chore: comment loader

--- apps/server/src/data-loader/decorators/loader.decorator.ts
+++ apps/server/src/data-loader/decorators/loader.decorator.ts
@@ -1,5 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+// Takes a string name of a desired loader.
 export const Loader = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const [, , { loaders }] = ctx.getArgs();
