0bb3bd7484543c511fb9a060688f181b72a6b306
apetrovic6 <petrovicante6@gmail.com>
2022-07-29 11:20:08 +0200

    chore: add DTOs for JwtPayload and Token object type

--- libs/entities/src/lib/user/index.ts
+++ libs/entities/src/lib/user/index.ts
@@ -2,3 +2,5 @@ export { User } from './entities/user.entity';
 export { CreateUserInput } from './dto/create-user.input';
 export { UpdateUserInput } from './dto/update-user.input';
 export { GetAuthArgs } from './dto/args/get-auth.args';
+export { JwtPayload } from './dto/jwt-payload';
+export { AuthOutput } from './dto/auth.dto';
