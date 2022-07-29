10d832c6115286717eadc44bb54091d21d4d2326
apetrovic6 <petrovicante6@gmail.com>
2022-07-29 22:14:11 +0200

    feat: implement create folder method

--- apps/server/src/folders/folders.service.ts
+++ apps/server/src/folders/folders.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { CreateFolderInput, UpdateFolderInput } from '@notes/entities/folders';
+import {
+  CreateFolderInput,
+  Folder,
+  UpdateFolderInput,
+} from '@notes/entities/folders';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { from, of, switchMap } from 'rxjs';
 
 @Injectable()
 export class FoldersService {
+  constructor(
+    @InjectRepository(Folder)
+    private readonly folderRepository: Repository<Folder>
+  ) {}
+
   create(createFolderInput: CreateFolderInput) {
-    return 'This action adds a new folder';
+    return from(of(this.folderRepository.create(createFolderInput))).pipe(
+      switchMap(folder => this.folderRepository.save(folder))
+    );
   }
 
   findAll() {
