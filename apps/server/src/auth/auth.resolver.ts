3e5f499b815e0800481622d7dafad58025e12f63
apetrovic6 <petrovicante6@gmail.com>
2022-07-29 11:21:41 +0200

    chore: Change return type

--- apps/server/src/auth/auth.resolver.ts
+++ apps/server/src/auth/auth.resolver.ts
@@ -1,17 +1,17 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { CreateUserInput, User } from '@notes/entities/user';
-import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { AuthOutput } from '@notes/entities/user';
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
-  @Mutation(() => User, { name: 'signup' })
+  @Mutation(() => AuthOutput, { name: 'signup' })
   signup(
     @Args('getAuthArgs', { type: () => CreateUserInput })
     userArgs: CreateUserInput
-  ): Observable<User> {
+  ) {
     return this.authService.signup(userArgs);
   }
 
