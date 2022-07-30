fcb5b8ac089a1ea9aef37f5fa22efebd1d736c2a
apetrovic6 <petrovicante6@gmail.com>
2022-07-30 02:04:04 +0200

    feat: add user decorator, add auth guard, changed id types

--- apps/server/src/folders/folders.resolver.ts
+++ apps/server/src/folders/folders.resolver.ts
@@ -1,41 +1,51 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { FoldersService } from './folders.service';
 import {
   Folder,
   CreateFolderInput,
   UpdateFolderInput,
 } from '@notes/entities/folders';
-
+import { Observable } from 'rxjs';
+import { User } from '../user/get-user.decorator';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '@notes/auth-helpers';
 @Resolver(() => Folder)
 export class FoldersResolver {
   constructor(private readonly foldersService: FoldersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => Folder)
   createFolder(
+    @User() user: { userId: string },
     @Args('createFolderInput') createFolderInput: CreateFolderInput
   ) {
-    return this.foldersService.create(createFolderInput);
+    return this.foldersService.create(createFolderInput, user.userId);
   }
 
   @Query(() => [Folder], { name: 'folders' })
-  findAll() {
-    return this.foldersService.findAll();
+  findAll(@Args('userId', { type: () => ID }) userId: string) {
+    return this.foldersService.findAll(userId);
   }
 
   @Query(() => Folder, { name: 'folder' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  findOne(@Args('id', { type: () => ID }) id: string): Observable<Folder> {
     return this.foldersService.findOne(id);
   }
 
   @Mutation(() => Folder)
   updateFolder(
+    @User() user: { userId: string },
     @Args('updateFolderInput') updateFolderInput: UpdateFolderInput
   ) {
-    return this.foldersService.update(updateFolderInput.id, updateFolderInput);
+    return this.foldersService.update(
+      updateFolderInput.id,
+      updateFolderInput,
+      user.userId
+    );
   }
 
   @Mutation(() => Folder)
-  removeFolder(@Args('id', { type: () => Int }) id: number) {
+  removeFolder(@Args('id', { type: () => ID }) id: string) {
     return this.foldersService.remove(id);
   }
 }
