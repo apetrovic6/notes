7433cd5a6dfedcbcbb796d5179b18931707e04e2
apetrovic6 <petrovicante6@gmail.com>
2022-07-30 02:00:27 +0200

    refactor: change field type

--- libs/entities/src/lib/folders/dto/update-folder.input.ts
+++ libs/entities/src/lib/folders/dto/update-folder.input.ts
@@ -1,8 +1,8 @@
 import { CreateFolderInput } from './create-folder.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, ID, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateFolderInput extends PartialType(CreateFolderInput) {
-  @Field(() => Int)
-  id: number;
+  @Field(() => ID)
+  id: string;
 }
