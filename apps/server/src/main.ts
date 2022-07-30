b2b22e00908f7504609141a1130ce559354c2876
apetrovic6 <petrovicante6@gmail.com>
2022-07-30 17:17:40 +0200

    chore: implement cookieParser

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,13 +5,16 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  app.use(cookieParser());
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
