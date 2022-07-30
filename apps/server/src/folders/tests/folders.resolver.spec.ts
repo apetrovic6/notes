ebdf08a96723abcb61637062c8e00bbfaf9e5169
apetrovic6 <petrovicante6@gmail.com>
2022-07-30 02:02:46 +0200

    test: add folder resolver tests

--- apps/server/src/folders/tests/folders.resolver.spec.ts
+++ apps/server/src/folders/tests/folders.resolver.spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FoldersResolver } from '../folders.resolver';
 import { FoldersService } from '../folders.service';
-import { CreateFolderInput, Folder } from '@notes/entities/folders';
+import {
+  CreateFolderInput,
+  Folder,
+  UpdateFolderInput,
+} from '@notes/entities/folders';
 import { userStub } from '../../user/tests/stubs/user.stub';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { folderStub } from './stubs/folderStub';
-import { of } from 'rxjs';
+import { NotFoundError, of } from 'rxjs';
+import { UnauthorizedException } from '@nestjs/common';
 
 jest.mock('../folders.service');
 
@@ -43,11 +48,12 @@ describe('FoldersResolver', () => {
 
     const folderInput: CreateFolderInput = {
       title: 'Test Folder',
-      user: userStub,
     };
 
     beforeEach(done => {
-      resolver.createFolder(folderInput).subscribe(res => (folder = res));
+      resolver
+        .createFolder(userStub.id, folderInput)
+        .subscribe(res => (folder = res));
       done();
     });
 
@@ -56,7 +62,117 @@ describe('FoldersResolver', () => {
     });
 
     it('It should be called with the correct arguments', () => {
-      expect(service.create).toBeCalledWith(folderInput);
+      service.create(folderInput, userStub.id);
+
+      expect(service.create).toBeCalledWith(folderInput, userStub.id);
+    });
+
+    it('It should return the created folder', () => {
+      expect(folder).toEqual(folderStub);
+    });
+
+    describe('When findAll is called', () => {
+      let folders: Folder[];
+
+      beforeEach(done => {
+        resolver.findAll(userStub.id).subscribe(res => (folders = res));
+        done();
+      });
+
+      it('It should be defined', () => {
+        expect(resolver.findAll).toBeDefined();
+      });
+
+      it("Should be called with the user's id", () => {
+        expect(service.findAll).toBeCalledWith(userStub.id);
+      });
+
+      it("It should return the user's folders", () => {
+        expect(folders).toEqual([folderStub, folderStub]);
+      });
+    });
+    describe('When findOne is called', () => {
+      let folder: Folder;
+      beforeEach(done => {
+        resolver.findOne(folderStub.id).subscribe(res => (folder = res));
+        done();
+      });
+
+      it('It should be defined', () => {
+        expect(resolver.findOne).toBeDefined();
+      });
+
+      it("It should be called with the folder's id", () => {
+        expect(service.findOne).toBeCalledWith(folderStub.id);
+      });
+
+      it('It should return the folder', () => {
+        expect(folder).toEqual(folderStub);
+      });
+
+      it("It should throw an error if the folder doesn't exist", () => {
+        jest.spyOn(service, 'findOne').mockReturnValue(of(null));
+        resolver
+          .findOne(folderStub.id)
+          .subscribe(res => expect(res).toBeNull());
+      });
+    });
+
+    describe('When updateFolder is called', () => {
+      let folder: Folder;
+      const folderInput: UpdateFolderInput = {
+        id: folderStub.id,
+        title: 'Test Folder',
+      };
+
+      beforeEach(done => {
+        resolver
+          .updateFolder(userStub.id, folderInput)
+          .subscribe(res => (folder = res));
+        done();
+      });
+
+      it('It should be defined', () => {
+        expect(resolver.updateFolder).toBeDefined();
+      });
+
+      it("It should be called with the folder's id", () => {
+        service.update(folderInput.id, folderInput, userStub.id);
+        expect(service.update).toBeCalledWith(
+          folderInput.id,
+          folderInput,
+          userStub.id
+        );
+      });
+
+      it('It should return the updated folder', () => {
+        expect(folder).toEqual(folderStub);
+      });
+
+      it("It should throw an error if the folder doesn't exist", () => {
+        jest.spyOn(service, 'update').mockImplementationOnce(() => {
+          throw new NotFoundError("Folder doesn't exist");
+        });
+
+        expect(() =>
+          resolver.updateFolder(userStub.id, folderInput)
+        ).toThrowError(NotFoundError);
+      });
+
+      it("It should throw an error if the folder doesn't belong to the user", () => {
+        jest.spyOn(service, 'update').mockImplementationOnce(() => {
+          throw new UnauthorizedException("Folder doesn't belong to user");
+        });
+        expect(() =>
+          resolver.updateFolder(userStub.id, folderInput)
+        ).toThrowError(UnauthorizedException);
+      });
+    });
+
+    describe('When removeFolder is called', () => {
+      it('It should be defined', () => {
+        expect(resolver.removeFolder).toBeDefined();
+      });
     });
   });
 });
