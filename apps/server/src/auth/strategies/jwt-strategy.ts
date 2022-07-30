4e76ab73149473cfe2ca0fd035f13cbddbfd9254
apetrovic6 <petrovicante6@gmail.com>
2022-07-30 17:22:10 +0200

    refactor: Strategy now extracts jwt from a cookie

--- apps/server/src/auth/strategies/jwt-strategy.ts
+++ apps/server/src/auth/strategies/jwt-strategy.ts
@@ -3,12 +3,15 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { JwtPayload } from '@notes/entities/user';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(protected configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => req.cookies?.Authorization,
+      ]),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_SECRET'),
     });
