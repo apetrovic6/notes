ec4489b2fa627720402d848fc1e059401d4bc21d
apetrovic6 <petrovicante6@gmail.com>
2022-07-30 17:17:12 +0200

    feat: if there's no loader name supplied to the loader decorator, it will return all of them.

--- apps/server/src/data-loader/decorators/loader.decorator.ts
+++ apps/server/src/data-loader/decorators/loader.decorator.ts
@@ -2,9 +2,9 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 // Takes a string name of a desired loader.
 export const Loader = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
+  (data: string | null, ctx: ExecutionContext) => {
     const [, , { loaders }] = ctx.getArgs();
 
-    return loaders[data];
+    return data ? loaders[data] : loaders;
   }
 );
