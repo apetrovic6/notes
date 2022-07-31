7f4a2d20be1bc67480ae3ecb8229b60db21cd962
apetrovic6 <petrovicante6@gmail.com>
2022-07-31 16:39:32 +0200

    chore: setup mantine

--- apps/front-react/pages/_app.tsx
+++ apps/front-react/pages/_app.tsx
@@ -1,15 +1,22 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { MantineProvider } from '@mantine/core';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Welcome to front-react!</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       <main className="app">
-        <Component {...pageProps} />
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <Component {...pageProps} />
+        </MantineProvider>
       </main>
     </>
   );
