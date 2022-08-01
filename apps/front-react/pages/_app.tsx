9d8178716911b3ca7744720fbb10730d13f12370
apetrovic6 <petrovicante6@gmail.com>
2022-08-01 11:37:51 +0200

    feat: set up a prototype app shell

--- apps/front-react/pages/_app.tsx
+++ apps/front-react/pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import {
+  AppShell,
+  Header,
+  MantineProvider,
+  Menu,
+  Text,
+  ActionIcon,
+  Avatar,
+  Burger,
+} from '@mantine/core';
+
 import './styles.css';
 
 import { ReactElement, ReactNode } from 'react';
@@ -28,7 +39,46 @@ function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
       </Head>
       <main className="app">
         <MantineProvider withGlobalStyles withNormalizeCSS>
-          <Component {...pageProps} />
+          <AppShell
+            padding={-1000}
+            header={
+              <Header
+                height={50}
+                p={15}
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                }}
+              >
+                <span style={{ display: 'flex', alignItems: 'center' }}>
+                  <Burger opened={false} onClick={} />
+                  <Text
+                    mx={10}
+                    variant={'gradient'}
+                    size={'xl'}
+                    gradient={{ from: 'yellow', to: 'green', deg: 50 }}
+                  >
+                    Notes
+                  </Text>
+                </span>
+                <Menu width={200} shadow="md" radius={'md'}>
+                  <Menu.Target>
+                    <ActionIcon>
+                      <Avatar radius={'xl'} />
+                    </ActionIcon>
+                  </Menu.Target>
+                  <Menu.Dropdown>
+                    <Menu.Item component={NextLink} href={'/auth/login'}>
+                      Login
+                    </Menu.Item>
+                  </Menu.Dropdown>
+                </Menu>
+              </Header>
+            }
+          >
+            <Component {...pageProps} />
+          </AppShell>
         </MantineProvider>
       </main>
     </>
