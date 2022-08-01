ee34b66880ddcdb1704c4e3695a914a6e6d276bb
apetrovic6 <petrovicante6@gmail.com>
2022-08-01 11:36:55 +0200

    chore: set up a custom layout

--- apps/front-react/pages/_app.tsx
+++ apps/front-react/pages/_app.tsx
@@ -1,10 +1,23 @@
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
+import type { NextPage } from 'next';
 import Head from 'next/head';
 import './styles.css';
-import { MantineProvider } from '@mantine/core';
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return (
+import { ReactElement, ReactNode } from 'react';
+import { NextLink } from '@mantine/next';
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? (page => page);
+
+  return getLayout(
     <>
       <Head>
         <title>Welcome to front-react!</title>
