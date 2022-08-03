18f4eb1ab8d333070dc1eab30617ab6465048669
apetrovic6 <petrovicante6@gmail.com>
2022-08-03 02:16:28 +0200

    refactor: delete unneeded hooks and imports

--- apps/front-react/components/Navbar.tsx
+++ apps/front-react/components/Navbar.tsx
@@ -2,13 +2,12 @@ import { NavLink, Navbar as MNavbar } from '@mantine/core';
 import { useSelector } from '@notes/store';
 
 export const Navbar = () => {
-  const sidebarOpen = useSelector(state => state.ui.sidebarOpen);
   return (
     <MNavbar
       p="md"
       className={`transition-all duration-300 ease-in-out `}
       // hiddenBreakpoint="sm"
-      hidden={sidebarOpen}
+
       width={{ sm: 200, lg: 300 }}
     >
       <NavLink label="First parent link" childrenOffset={28}>
