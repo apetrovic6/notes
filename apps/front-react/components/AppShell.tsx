3d02a14fcc33ff45c40e899a8846922b55bd36cc
apetrovic6 <petrovicante6@gmail.com>
2022-08-03 19:02:02 +0200

    feat: start populating navbar and header with data

--- apps/front-react/components/AppShell.tsx
+++ apps/front-react/components/AppShell.tsx
@@ -5,6 +5,7 @@ import {
   Header,
   Footer,
   Aside,
+  NavLink,
   Box,
   Text,
   MediaQuery,
@@ -13,14 +14,20 @@ import {
   Menu,
   ActionIcon,
   Avatar,
+  Skeleton,
 } from '@mantine/core';
 import { useRouter } from 'next/router';
 import { NextLink } from '@mantine/next';
+import { useGetFoldersQuery, useMeQuery } from '@notes/apollo';
 
 export default function AppShell({ children }) {
   const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
   const { pathname } = useRouter();
+
+  const { data, loading } = useGetFoldersQuery();
+
+  const { data: userData } = useMeQuery();
   return (
     <MantineAppShell
       styles={{
@@ -41,7 +48,26 @@ export default function AppShell({ children }) {
             hidden={!opened}
             width={{ sm: 200, lg: 300 }}
           >
-            <Text>Application navbar</Text>
+            <Text>Folders</Text>
+
+            {loading && (
+              <>
+                <Skeleton height={8} radius="xl" />
+                <Skeleton height={8} mt={6} radius="xl" />
+                <Skeleton height={8} mt={6} width="70%" radius="xl" />
+              </>
+            )}
+
+            {data?.folders &&
+              data?.folders?.map(folder => (
+                <NavLink
+                  label={folder.title}
+                  key={folder.id}
+                  childrenOffset={28}
+                >
+                  <NavLink label="First child link" />
+                </NavLink>
+              ))}
           </Navbar>
         )
       }
@@ -88,8 +114,8 @@ export default function AppShell({ children }) {
                 Notes
               </Text>
             </Box>
-            <span>
-              {' '}
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Text mr={10}> Welcome {userData?.me?.email.split('@')[0]}</Text>
               <Menu width={200} shadow="md" radius={'md'}>
                 <Menu.Target>
                   <ActionIcon>
@@ -97,12 +123,19 @@ export default function AppShell({ children }) {
                   </ActionIcon>
                 </Menu.Target>
                 <Menu.Dropdown>
-                  <Menu.Item component={NextLink} href={'/auth/login'}>
-                    Login
-                  </Menu.Item>
+                  {!userData && (
+                    <Menu.Item component={NextLink} href={'/auth/login'}>
+                      Login
+                    </Menu.Item>
+                  )}
+                  {userData && (
+                    <Menu.Item component={NextLink} href={'/auth/logout'}>
+                      Logout
+                    </Menu.Item>
+                  )}
                 </Menu.Dropdown>
               </Menu>
-            </span>
+            </Box>
           </div>
         </Header>
       }
