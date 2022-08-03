55f8f9dc4323ffb8ad272718bcbb29278b03f240
apetrovic6 <petrovicante6@gmail.com>
2022-08-03 13:49:57 +0200

    fix: fix the returning error issue

--- libs/store/src/lib/api/base.ts
+++ libs/store/src/lib/api/base.ts
@@ -8,6 +8,26 @@ export const client = new GraphQLClient('http://localhost:3333/graphql', {
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: graphqlRequestBaseQuery({ client }),
+  baseQuery: graphqlRequestBaseQuery({
+    client,
+    customErrors: error => {
+      const {
+        response: { errors },
+      } = error;
+
+      // @ts-ignore
+      const [
+        {
+          extensions: { response },
+        },
+      ] = errors;
+
+      return {
+        name: response.error,
+        message: response.message,
+        stack: response.stack,
+      };
+    },
+  }),
   endpoints: () => ({}),
 });
