2a01f2619c91d0c722b0f4ae4eca41fb9ac70d4c
apetrovic6 <petrovicante6@gmail.com>
2022-08-04 11:51:19 +0200

    refactor: remove unneeded import

--- apps/front-react/pages/dashboard/new/index.tsx
+++ apps/front-react/pages/dashboard/new/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { RichTextEditor } from '@mantine/rte';
 import { useRouter } from 'next/router';
 import { CreateNoteInput } from '@notes/entities/notes';
