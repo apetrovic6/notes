902b63091c08d4b27456c5c4f5d6ca3ca4ca5b51
apetrovic6 <petrovicante6@gmail.com>
2022-08-04 22:13:04 +0200

    chore: testing the implementation

--- apps/front-react/components/folder-list/folder-list.component.tsx
+++ apps/front-react/components/folder-list/folder-list.component.tsx
@@ -1,29 +1,54 @@
-import { Box, NavLink } from '@mantine/core';
+import { Box, Menu, NavLink } from '@mantine/core';
 import { NoteList } from './note-list.component';
+import { IconDotsVertical, IconTrash } from '@tabler/icons';
 
 export const FolderList = ({ folders }) => {
   return (
     <>
       {folders?.map(folder => (
-        <Box key={folder.id} sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box
+          key={folder.id}
+          sx={{ display: 'flex', alignItems: 'center', position: 'relative' }}
+        >
+          <Menu trigger={'hover'}>
+            <Menu.Target>
+              <IconDotsVertical
+                style={{
+                  marginRight: -10,
+                  marginLeft: -15,
+                  position: 'absolute',
+                  top: 6,
+                }}
+              />
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item icon={<IconTrash size={15} />}>Delete</Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+
           <Box
-            sx={{
-              display: 'flex',
-              width: '100%',
-              position: 'relative',
-              alignItems: 'center',
-            }}
+            key={folder.id}
+            sx={{ display: 'flex', alignItems: 'center', width: '100%' }}
           >
-            <div style={{ display: 'block', width: '90%' }}>
-              <NavLink
-                variant={'filled'}
-                label={folder.title}
-                key={folder.id}
-                childrenOffset={20}
-              >
-                <NoteList notes={folder.notes} folderId={folder.id} />
-              </NavLink>
-            </div>
+            <Box
+              sx={{
+                display: 'flex',
+                width: '100%',
+                position: 'relative',
+                alignItems: 'center',
+              }}
+            >
+              <div style={{ display: 'block', width: '90%' }}>
+                <NavLink
+                  variant={'filled'}
+                  label={folder.title}
+                  key={folder.id}
+                  childrenOffset={20}
+                >
+                  <NoteList notes={folder.notes} folderId={folder.id} />
+                </NavLink>
+              </div>
+            </Box>
           </Box>
         </Box>
       ))}
