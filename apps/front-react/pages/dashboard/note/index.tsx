bc990101311c431872bea906fe10a6881dbc8ac6
apetrovic6 <petrovicante6@gmail.com>
2022-08-06 00:13:30 +0200

    feat: add update note functionality

--- apps/front-react/pages/dashboard/note/index.tsx
+++ apps/front-react/pages/dashboard/note/index.tsx
@@ -1,26 +1,111 @@
-import { GetNoteQueryResult, useGetNoteQuery } from '@notes/apollo';
+import { FC, useEffect, useRef, useState } from 'react';
+import RichTextEditor from '../../../components/RichTextEditor';
 import { useRouter } from 'next/router';
-import { LoadingOverlay } from '@mantine/core';
+import { CreateNoteInput } from '@notes/entities/notes';
 
-const Note = () => {
+import { useGetNoteQuery, useUpdateNoteMutation } from '@notes/apollo';
+import { LoadingOverlay, TextInput } from '@mantine/core';
+import { Editor } from '@mantine/rte';
+import { showNotification } from '@mantine/notifications';
+
+interface INewNote extends Omit<CreateNoteInput, 'folder'> {
+  id: string;
+  folder: { id: string };
+}
+
+export const NewNote: FC<INewNote> = () => {
+  const { query } = useRouter();
+  const editorRef = useRef<Editor>();
+
   const {
-    query: { noteId },
-  } = useRouter();
-  const { data, loading, error } = useGetNoteQuery({
-    variables: { id: noteId as string },
+    data,
+    loading,
+    error: getError,
+  } = useGetNoteQuery({
+    variables: { id: query.noteId as string },
   });
+
+  const [fullNote, setFullNote] = useState({
+    ...data?.note,
+  });
+  const [caret, setCaret] = useState<number>();
+
+  const [updateNote] = useUpdateNoteMutation({
+    variables: {
+      updateNoteInput: {
+        id: fullNote.id,
+        title: fullNote.title,
+        content: fullNote.content,
+      },
+    },
+  });
+
+  useEffect(() => {
+    // Sets new note data
+    setFullNote(() => ({ ...data?.note }));
+
+    // Force new note content into the rich text editor (Because the editor is shite)
+    editorRef.current?.editor?.clipboard.dangerouslyPasteHTML(
+      data?.note?.content ?? ''
+    );
+
+    // Set the caret to the last position before the note updated
+    if (caret) {
+      editorRef?.current?.editor?.setSelection({
+        index: caret,
+        length: 0,
+      });
+    }
+  }, [data, editorRef.current?.editor, data?.note?.content]);
+
+  useEffect(() => {
+    const test = setTimeout(() => {
+      // Get and set the caret state before it updates
+      setCaret(editorRef?.current?.editor.getSelection()?.index);
+      if (fullNote?.id) {
+        updateNote();
+      }
+    }, 800);
+
+    return () => {
+      clearTimeout(test);
+    };
+  }, [fullNote.title, fullNote.content]);
 
   if (loading) {
     return <LoadingOverlay visible={loading} />;
   }
 
-  return (
-    <div>
-      <h1>{data?.note?.title}</h1>
+  if (getError) {
+    showNotification({
+      title: getError.name,
+      message: getError.message,
+      color: 'red',
+    });
+  }
 
-      <div dangerouslySetInnerHTML={{ __html: data?.note?.content }} />
-    </div>
+  return (
+    <>
+      <TextInput
+        placeholder="Title"
+        label="Title"
+        radius="xl"
+        size="lg"
+        value={fullNote.title}
+        my={20}
+        onChange={e =>
+          setFullNote(state => ({ ...state, title: e.target.value }))
+        }
+        required
+      />
+      <RichTextEditor
+        editorRef={editorRef}
+        radius={'xl'}
+        value={fullNote.content}
+        onChange={e => setFullNote(state => ({ ...state, content: e }))}
+      />
+    </>
   );
 };
 
-export default Note;
+export default NewNote;
