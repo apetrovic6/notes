552cbe1c1e1b13d03c30a2c580fd54cffa3eb53c
apetrovic6 <petrovicante6@gmail.com>
2022-08-09 01:17:45 +0200

    refactor: remove unused variables

--- libs/entities/src/lib/base/base.entity.ts
+++ libs/entities/src/lib/base/base.entity.ts
@@ -7,7 +7,7 @@ import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class BaseEntity {
-  @Field(returns => ID, { name: 'id', description: 'Unique identifier' })
+  @Field(() => ID, { name: 'id', description: 'Unique identifier' })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
