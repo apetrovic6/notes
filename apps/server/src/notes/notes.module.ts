a9b4dfd3125cc168195f73ba4b7cc5481ac67089
apetrovic6 <petrovicante6@gmail.com>
2022-08-11 01:33:36 +0200

    feat: inject pubSub providers

--- apps/server/src/notes/notes.module.ts
+++ apps/server/src/notes/notes.module.ts
@@ -4,10 +4,18 @@ import { NotesResolver } from './notes.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Note } from '@notes/entities/notes';
 import { UserModule } from '../user/user.module';
+import { PubSub } from 'graphql-subscriptions';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Note]), UserModule],
-  providers: [NotesResolver, NotesService],
+  providers: [
+    NotesResolver,
+    NotesService,
+    {
+      provide: 'PUB_SUB',
+      useValue: new PubSub(),
+    },
+  ],
   exports: [NotesService],
 })
 export class NotesModule {}
