63225beb4bea5b43079cf691e781e717be24b929
apetrovic6 <petrovicante6@gmail.com>
2022-08-11 01:32:10 +0200

    feat: setup guard to authenticate subscription req

--- libs/auth-helpers/src/lib/guards/jwt.guard.ts
+++ libs/auth-helpers/src/lib/guards/jwt.guard.ts
@@ -5,6 +5,29 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context).getContext();
+
+    // TODO refactor
+
+    // graphql-ws
+    if (ctx.req.upgrade === 'websocket') {
+      return {
+        ...ctx,
+        cookies: {
+          Authorization: ctx.req.cookie.split('Authorization=')[1],
+        },
+      };
+    }
+
+    // The other one
+    if (ctx?.req?.headers?.upgrade === 'websocket') {
+      return {
+        ...ctx,
+        cookies: {
+          Authorization: ctx.headers.cookie.split('Authorization=')[1],
+        },
+      };
+    }
     return GqlExecutionContext.create(context).getContext().req;
   }
 }
