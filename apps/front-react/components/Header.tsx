d5de52c99afc518cd71b67beaa97bd303d7c5223
apetrovic6 <petrovicante6@gmail.com>
2022-08-11 17:56:36 +0200

    refactor: extract header and navbar into their own components

---
+++ apps/front-react/components/Header.tsx
@@ -0,0 +1,117 @@
+import {
+  ActionIcon,
+  Avatar,
+  Box,
+  Burger,
+  MediaQuery,
+  Menu,
+  Text,
+  useMantineColorScheme,
+  useMantineTheme,
+} from '@mantine/core';
+import { NextLink } from '@mantine/next';
+import { IconCheck, IconMoon, IconSunHigh } from '@tabler/icons';
+import { Header as MantineHeader } from '@mantine/core';
+import { useReactiveVar } from '@apollo/client';
+import { loggedIn, loggedUser } from '../lib/apollo';
+import { useRouter } from 'next/router';
+import { showNotification } from '@mantine/notifications';
+import { useLogoutMutation } from '@notes/apollo';
+
+export const Header = ({ opened, setOpened }) => {
+  const { pathname, replace } = useRouter();
+
+  const loggedInR = useReactiveVar(loggedIn);
+  const userR = useReactiveVar(loggedUser);
+
+  const theme = useMantineTheme();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+
+  const [logout, { client, data: logoutCall }] = useLogoutMutation();
+
+  const onLogout = () => {
+    logout();
+    client.clearStore();
+
+    loggedUser(null);
+    loggedIn(null);
+
+    replace('/auth/login');
+    logoutCall &&
+      showNotification({
+        title: 'Success',
+        message: "You've successfully signed out",
+        color: 'teal',
+        icon: <IconCheck size={18} />,
+      });
+  };
+
+  return (
+    <MantineHeader height={70} p="md">
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          height: '100%',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {pathname.includes('dashboard') && (
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened(o => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+          )}
+
+          <Text
+            variant={'gradient'}
+            size={'xl'}
+            gradient={{ from: 'yellow', to: 'green', deg: 50 }}
+            sx={{ userSelect: 'none' }}
+          >
+            Notes
+          </Text>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {loggedInR && (
+            <MediaQuery smallerThan={'xs'} styles={{ display: 'none' }}>
+              <Text mr={10}>Welcome {userR?.email.split('@')[0]}</Text>
+            </MediaQuery>
+          )}
+          <Menu width={200} shadow="md" radius={'md'}>
+            <Menu.Target>
+              <ActionIcon>
+                <Avatar radius={'xl'} />
+              </ActionIcon>
+            </Menu.Target>
+            <Menu.Dropdown>
+              {!loggedInR && (
+                <Menu.Item component={NextLink} href={'/auth/login'}>
+                  Login
+                </Menu.Item>
+              )}
+              {loggedInR && <Menu.Item onClick={onLogout}>Logout</Menu.Item>}
+            </Menu.Dropdown>
+          </Menu>
+          <ActionIcon
+            mx={10}
+            variant="subtle"
+            color={dark ? 'orange' : 'blue'}
+            radius={'xl'}
+            onClick={() => toggleColorScheme()}
+            title="Toggle color scheme"
+          >
+            {dark ? <IconSunHigh size={28} /> : <IconMoon size={28} />}
+          </ActionIcon>
+        </Box>
+      </div>
+    </MantineHeader>
+  );
+};
--- apps/front-react/components/Navbar.tsx
+++ apps/front-react/components/Navbar.tsx
@@ -1,29 +1,98 @@
-import { NavLink, Navbar as MNavbar } from '@mantine/core';
+import {
+  NavLink,
+  Navbar as MNavbar,
+  Button,
+  SegmentedControl,
+  Skeleton,
+} from '@mantine/core';
+import { FolderList } from './folder-list/folder-list.component';
+import { NoteList } from './folder-list/note-list.component';
+import { openModal } from '@mantine/modals';
+import { CreateUpdateFolder } from './CreateUpdateFolder';
+import {
+  GetFoldersDocument,
+  useCreateFolderMutation,
+  useGetFoldersQuery,
+  useGetSharedNotesQuery,
+} from '@notes/apollo';
+import { useState } from 'react';
 
-export const Navbar = () => {
+export const Navbar = ({ opened }) => {
+  const [showShared, setShowShared] = useState('my-notes');
+  const { data, loading } = useGetFoldersQuery();
+  const { data: sharedNotes } = useGetSharedNotesQuery();
+
+  const [createFolder] = useCreateFolderMutation({
+    refetchQueries: [GetFoldersDocument],
+  });
+
+  const onCreateFolder = () => {
+    openModal({
+      title: 'Create Folder',
+      children: (
+        <>
+          <CreateUpdateFolder
+            mutation={() => createFolder}
+            payload={{
+              variables: { createFolderInput: { title: null } },
+            }}
+          />
+        </>
+      ),
+    });
+  };
+
+  const segmentData = [
+    {
+      label: 'My notes',
+      value: 'my-notes',
+    },
+    {
+      label: 'Shared',
+      value: 'shared',
+    },
+  ];
+
   return (
     <MNavbar
       p="md"
-      className={`transition-all duration-300 ease-in-out `}
-      // hiddenBreakpoint="sm"
-
-      width={{ sm: 200, lg: 300 }}
+      hiddenBreakpoint="sm"
+      hidden={!opened}
+      width={{ sm: 200, lg: 310 }}
     >
-      <NavLink label="First parent link" childrenOffset={28}>
-        <NavLink label="First child link" />
-        <NavLink label="Second child link" />
-        <NavLink label="Nested parent link" childrenOffset={28}>
-          <NavLink label="First child link" />
-          <NavLink label="Second child link" />
-          <NavLink label="Third child link" />
-        </NavLink>
-      </NavLink>
+      <Button
+        fullWidth
+        mb={10}
+        variant="light"
+        radius="lg"
+        onClick={onCreateFolder}
+      >
+        Create Folder
+      </Button>
 
-      <NavLink label="Second parent link" childrenOffset={28}>
-        <NavLink label="First child link" />
-        <NavLink label="Second child link" />
-        <NavLink label="Third child link" />
-      </NavLink>
+      <SegmentedControl
+        value={showShared}
+        onChange={setShowShared}
+        color={'blue'}
+        radius={'lg'}
+        data={segmentData}
+      />
+
+      {loading && (
+        <>
+          <Skeleton height={15} radius="xl" />
+          <Skeleton height={15} mt={10} radius="xl" />
+          <Skeleton height={15} mt={10} radius="xl" />
+        </>
+      )}
+
+      {showShared === 'my-notes' && data?.folders && (
+        <FolderList folders={data.folders} />
+      )}
+
+      {showShared === 'shared' && sharedNotes && (
+        <NoteList notes={sharedNotes?.getNotesForCollaborator} />
+      )}
     </MNavbar>
   );
 };
