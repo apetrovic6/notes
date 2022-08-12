66bb03d72513c38fb8d8d05f4ed0192d8c4e5cfc
apetrovic6 <petrovicante6@gmail.com>
2022-08-13 01:49:13 +0200

    feat: add shared and collaborators field to note input

--- libs/entities/src/lib/notes/dto/update-note.input.ts
+++ libs/entities/src/lib/notes/dto/update-note.input.ts
@@ -1,8 +1,20 @@
 import { CreateNoteInput } from './create-note.input';
 import { InputType, Field, PartialType, ID } from '@nestjs/graphql';
+import { CollabInput } from '../../user/dto/collab.input';
+import { User } from '../../user/entities/user.entity';
 
 @InputType()
 export class UpdateNoteInput extends PartialType(CreateNoteInput) {
   @Field(() => ID, { name: 'id', description: 'Unique identifier' })
   id: string;
+
+  @Field({ nullable: true })
+  shared: boolean;
+
+  @Field(() => [CollabInput], {
+    nullable: true,
+    name: 'collaborators',
+    description: 'Array of the collaborators',
+  })
+  collaborators: User[];
 }
