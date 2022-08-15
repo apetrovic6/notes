a366e2e1b401c55639bf291d4b373fceba8e0e1a
apetrovic6 <petrovicante6@gmail.com>
2022-08-15 13:43:38 +0200

    feat: import pubsub module into notes module

--- apps/server/src/notes/notes.module.ts
+++ apps/server/src/notes/notes.module.ts
@@ -4,18 +4,11 @@ import { NotesResolver } from './notes.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Note } from '@notes/entities/notes';
 import { UserModule } from '../user/user.module';
-import { PubSub } from 'graphql-subscriptions';
+import { PubSubModule } from '../pubsub/pub-sub.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Note]), UserModule],
-  providers: [
-    NotesResolver,
-    NotesService,
-    {
-      provide: 'PUB_SUB',
-      useValue: new PubSub(),
-    },
-  ],
+  imports: [TypeOrmModule.forFeature([Note]), UserModule, PubSubModule],
+  providers: [NotesResolver, NotesService],
   exports: [NotesService],
 })
 export class NotesModule {}
